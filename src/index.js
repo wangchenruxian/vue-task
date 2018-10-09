d42b8131266640287047d6ee3923bc0e36e18535
xianxian <email@example.com>
2018-10-09 10:24:21 +0800

    log

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: name
+* @Date:   2018-09-30 15:22:16
+* @Last Modified by:   name
+* @Last Modified time: 2018-09-30 15:34:52
+*/
+
+'use strict';
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path=require("path");
+moudle.exports={
+    moudle:"development",
+    entry:{
+        entry:"./src/index.js"
+    },
+    output:{
+        path:path.resolve(__dirname,"./dist"),
+        filename:"main.js"
+    },
+    moudle:{},
+    plugins:[],
+    devServer:{
+        contentBase:path.resolve(__dirname,"./dist"),
+        host:"localhost",
+        port:"8080",
+        compress:true
+    }
+}
